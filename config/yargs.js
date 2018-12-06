d1b0201d0f526cca1f6ad0752efce55c86baccf9
cdgomez <cgomez@smartsoftint.com>
2018-12-06 09:09:30 -0500

    Commit inicial

---
+++ config/yargs.js
@@ -0,0 +1,17 @@
+const opts = {
+    base: {
+        demand: true,
+        alias: 'b'
+    },
+    limite: {
+        alias: 'l',
+        default: 10
+    }
+}
+
+const argv = require('yargs')
+    .command('listar', `Imprime en consola la tabla de multiplicar`, opts)
+    .command('crear', 'Imprime en txt la tabla de multiplicar', opts)
+    .argv;
+
+module.exports = { argv }
